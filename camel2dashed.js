187e67973a550a21c9e7a14072a7f87bdce8f879
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-11-10 15:56:40 +0100

    Create camel2dashed.js

---
+++ camel2dashed.js
@@ -0,0 +1,7 @@
+const camel2dashed=s=>s.replace(/[^A-Z\s][A-Z]/g,t=>[t[0],t[1]].join('-')).toLowerCase();
+
+/*
+console.log(camel2dashed('CamelCaseText or CAmelCAseTExt or CAMelCaseTExt '));
+camel-case-text or camel-case-text or camel-case-text
+nb: broken with surrogate pair.
+*/
